c2cf19bc288cc54f17a53d46c65c7f5b8a41e5e7
ryanwest6 <ryanwest6@gmail.com>
2021-02-01 22:17:48 -0700

    try this
    Signed-off-by: ryanwest6 <ryanwest6@gmail.com>

--- scripts/free5gc.sh
+++ scripts/free5gc.sh
@@ -35,8 +35,14 @@ sudo systemctl start mongodb
 
 echo "4. Install User-Plane Supporting packages"
 sudo apt -yq update
+
+sudo apt remove cmdtest
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update
 # added yarn
-sudo apt -yq install git gcc cmake autoconf libtool pkg-config libmnl-dev libyaml-dev yarn
+sudo apt -yq install git gcc cmake autoconf libtool pkg-config libmnl-dev libyaml-dev yarn nodejs
+
 echo "4a. go get"
 env
 go env
